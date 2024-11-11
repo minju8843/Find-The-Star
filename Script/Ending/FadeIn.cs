715d6a813c4bb9f1f896c577e7439139a13558fc
minju8843 <74241186+minju8843@users.noreply.github.com>
2024-11-11 23:16:28 +0900

    Add files via upload

---
+++ Script/Ending/FadeIn.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeIn : MonoBehaviour
+{
+
+    public GameObject fadeInPanel;
+
+    private void Awake()
+    {
+        if (fadeInPanel != null)
+        {
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
+
+    }
+
+    private void Update()
+    {
+        
+    }
+}
